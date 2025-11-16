23c8b10bb64b9dd21766ff63b42152c4260ef508
sid <bhosalesid777@gmail.com>
2025-11-16 14:26:35 +0530

    new file

---
+++ basic_questions/SwapTwoNumbers/SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+
+
+public class SwapTwoNumbers {
+
+    public static void main(String[] args) {
+        
+        int a= 10;
+        int b = 20;
+
+        a = a+b ;
+        b = a-b;
+        a = a-b;
+
+        System.out.println("the new values of a and b are " + a + " "+ b );
+    }
+}
